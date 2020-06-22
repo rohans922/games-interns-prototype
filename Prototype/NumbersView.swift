0e24386c78246c65bf48b3ac6a983ef57aefd044
shandler mason <shandler.mason@nytimes.com>
2020-06-22 16:56:54 -0400

    second movement
    trying to implement movement of orange view

--- Prototype/NumbersView.swift
+++ Prototype/NumbersView.swift
@@ -12,10 +12,15 @@ class NumbersView: UIView {
     @IBOutlet var numbersView: UIView!
     
     @IBOutlet weak var image1: UIView!
-    @IBOutlet var oneGestureRecognizer: UIPanGestureRecognizer!
+    @IBOutlet weak var image2: UIView!
     
+    @IBOutlet var oneGestureRecognizer: UIPanGestureRecognizer!
+    @IBOutlet var twoGR: UIPanGestureRecognizer!
     
     //@IBOutlet var numbers: [UIView]!
+ 
+    var x_1 : Int = 0
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -34,16 +39,43 @@ class NumbersView: UIView {
         numbersView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
     }
     
+    
+    //added function to move (images) views
     @IBAction func didMoveOne(_ sender: UIPanGestureRecognizer) {
+        
         let translation = sender.translation(in: image1)
         image1.center.x += translation.x
         image1.center.y += translation.y
         sender.setTranslation(.zero, in: image1)
-        print(image1.center.x)
-        print(image1.center.y)
+//        print(image1.center.x)
+//        print(image1.center.y)
+        
+        x_1 = Int(image1.center.x)
+       
+        
+        let translation2 = sender.translation(in: image2)
+        image2.center.x += translation2.x
+        image2.center.y += translation2.y
+        sender.setTranslation(.zero, in: image2)
+//        print(image2.center.x)
+//        print(image2.center.y)
+        
+                
+        }
+    }
+
+    
+    // Function to swap 2 (image) views
+    
+
+func swapPlaces(xcoor:Int) {
+    
+    if(xcoor == 0){
+        
     }
     
+    }
+
+
     
-    // Add function to swap .image values in numbers array
-    
-}
+
